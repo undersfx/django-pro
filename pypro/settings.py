8034499c73ef7c29ad570f8f2e95e1c375eb7544
undersfx <atchin_@hotmail.com>
2020-10-10 14:46:01 -0300

    refactor: secret key in dynamic config

--- pypro/settings.py
+++ pypro/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l0(7!sgs8^%!h@&bgk*%c4n*54&e8rk8%k4ki3$p)ggzq)%pox'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
