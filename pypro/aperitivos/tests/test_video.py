b0e6b2ce979280edae5c864b35730775930335b8
undersfx <atchin_@hotmail.com>
2021-01-17 18:47:10 -0300

    add: App para videos aperitivos

---
+++ pypro/aperitivos/tests/test_video.py
@@ -0,0 +1,20 @@
+import pytest
+from django.urls import reverse
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture()
+def resp(client):
+    return client.get(reverse('aperitivos:video', args=('motivacao',)))
+
+
+def test_status_code(resp):
+    return resp.status_code == 200
+
+
+def test_video_title(resp):
+    assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>')
+
+
+def test_video_conteudo(resp):
+    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/501491219"')
--- pypro/urls.py
+++ pypro/urls.py
@@ -22,6 +22,7 @@ import debug_toolbar
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pypro.base.urls')),
+    path('aperitivos/', include('pypro.aperitivos.urls')),
 ]
 
 if settings.DEBUG:
