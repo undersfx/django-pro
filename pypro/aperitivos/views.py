1122658071b263135ca6366e2891622b24dcff34
undersfx <atchin_@hotmail.com>
2021-01-20 17:55:43 -0300

    add: Video de acordo com slug

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': '501491219'},
+        'instalacao': {'titulo': 'Video Aperitivo: Instalação Windows', 'vimeo_id': '502404083'}
+    }
+    contexto = videos[slug]
+    return render(request, 'aperitivos/video.html', context={'video': contexto})
