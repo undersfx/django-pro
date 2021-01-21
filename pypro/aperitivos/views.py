b3949108060708c85cbc1d7772e2cd04ae8b7a7c
undersfx <atchin_@hotmail.com>
2021-01-20 22:55:47 -0300

    add: Lista de videos aperitivo

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+videos = [
+    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': '501491219'},
+    {'slug': 'instalacao', 'titulo': 'Video Aperitivo: Instalação Windows', 'vimeo_id': '502404083'}
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
 
 def video(request, slug):
-    videos = {
-        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': '501491219'},
-        'instalacao': {'titulo': 'Video Aperitivo: Instalação Windows', 'vimeo_id': '502404083'}
-    }
-    contexto = videos[slug]
-    return render(request, 'aperitivos/video.html', context={'video': contexto})
+    video = videos_dct[slug]
+    return render(request, 'aperitivos/video.html', context={'video': video})
+
+
+def index(request):
+    return render(request, 'aperitivos/index.html', context={'videos': videos})
