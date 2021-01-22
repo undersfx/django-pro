921c2dd541e386bf00447ce3c12ec016164f7cb2
undersfx <atchin_@hotmail.com>
2021-01-21 22:11:59 -0300

    add: Link para aperitivos na home

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from django.urls import reverse
+
+
 def home(request):
-    return render(request, 'base/home.html')
+    contexto = {
+        'aperitivos': reverse('aperitivos:index')
+    }
+    return render(request, 'base/home.html', context={'contexto': contexto})
