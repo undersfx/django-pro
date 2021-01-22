212b418eb132dee5f7968875a6d886020a2cb7a7
undersfx <atchin_@hotmail.com>
2021-01-21 21:31:29 -0300

    add: Links para videos no indice

--- pypro/aperitivos/tests/test_indice.py
+++ pypro/aperitivos/tests/test_indice.py
@@ -20,3 +20,14 @@ def test_status_code(resp):
 )
 def test_video_title(resp, titulo):
     assert_contains(resp, titulo)
+
+
+@pytest.mark.parametrize(
+    'slug', [
+        'motivacao',
+        'instalacao'
+    ]
+)
+def test_video_title(resp, slug):
+    video_link = reverse('aperitivos:video', args=(slug,))
+    assert_contains(resp, f'href="{video_link}"')
