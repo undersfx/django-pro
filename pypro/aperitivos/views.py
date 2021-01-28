aa59ffc01d789a54ae1e25b97a73a685013036c5
undersfx <atchin_@hotmail.com>
2021-01-27 23:17:15 -0300

    add: Modelo de videos

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render
-from django.urls import reverse
-
-
-class Video:
-    def __init__(self, slug, titulo, vimeo_id):
-        self.slug = slug
-        self.titulo = titulo
-        self.vimeo_id = vimeo_id
-
-    def get_absolute_url(self):
-        return reverse('aperitivos:video', args=(self.slug,))
+from pypro.aperitivos.models import Video
 
 
 videos = [
-    Video('motivacao', 'Video Aperitivo: Motivação', '501491219'),
-    Video('instalacao', 'Video Aperitivo: Instalação Windows', '502404083')
+    Video(slug='motivacao', titulo='Video Aperitivo: Motivação', vimeo_id='501491219'),
+    Video(slug='instalacao', titulo='Video Aperitivo: Instalação Windows', vimeo_id='502404083')
 ]
 
 videos_dct = {v.slug: v for v in videos}
