fd89ca5b16d696281411f2b5c277ee18245b50f9
undersfx <atchin_@hotmail.com>
2021-01-30 02:54:08 -0300

    add: Testes de BD com Migrations

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,18 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from pypro.aperitivos.models import Video
 
 
-videos = [
-    Video(slug='motivacao', titulo='Video Aperitivo: Motivação', vimeo_id='501491219'),
-    Video(slug='instalacao', titulo='Video Aperitivo: Instalação Windows', vimeo_id='502404083')
-]
-
-videos_dct = {v.slug: v for v in videos}
-
-
 def video(request, slug):
     video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
 
 
 def index(request):
+    videos = Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/index.html', context={'videos': videos})
