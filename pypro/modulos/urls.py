6ef54478f2d9af0ee7bcaf6614b0fd77f0b24aee
undersfx <atchin_@hotmail.com>
2021-02-22 17:53:29 -0300

    add: Cria links para aulas

--- pypro/modulos/urls.py
+++ pypro/modulos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from pypro.modulos.views import detalhe
+from pypro.modulos.views import detalhe, aula
 
 
 app_name = 'modulos'
 
 urlpatterns = [
     path('<slug:slug>', detalhe, name='detalhe'),
+    path('aulas/<slug:slug>', aula, name='aula'),
 ]
