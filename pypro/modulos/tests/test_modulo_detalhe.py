65d8730f5eab0ead1c0eec1303463b8fbb30ac82
undersfx <atchin_@hotmail.com>
2021-02-22 17:53:29 -0300

    add: Cria links para aulas

--- pypro/modulos/tests/test_modulo_detalhe.py
+++ pypro/modulos/tests/test_modulo_detalhe.py
@@ -36,3 +36,8 @@ def test_descricao(resp, modulo: Modulo):
 def test_aulas_titulo(resp, aulas):
     for aula in aulas:
         assert_contains(resp, aula.titulo)
+
+
+def test_aulas_links(resp, aulas):
+    for aula in aulas:
+        assert_contains(resp, aula.get_absolute_url())
--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -7,3 +7,7 @@ def detalhe(request, slug):
     modulo = facade.encontrar_modulo(slug)
     aulas = facade.encontrar_aulas_por_modulo_ordenadas(modulo)
     return render(request, 'modulos/modulo_detalhe.html', context={'modulo': modulo, 'aulas': aulas})
+
+
+def aula(request, slug):
+    pass
