9e339e0310679c512f6eb8bb0b2af5b7697714e3
undersfx <atchin_@hotmail.com>
2021-03-07 19:30:36 -0300

    add|test: Redirecionamento após login

--- pypro/base/tests/test_login.py
+++ pypro/base/tests/test_login.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from model_bakery import baker
 
 
 @pytest.fixture()
@@ -10,3 +11,24 @@ def resp(client, db):
 
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+@pytest.fixture()
+def usuario(db, django_user_model):
+    _usuario = baker.make(django_user_model)
+    senha = 'senha'
+    _usuario.set_password(senha)
+    _usuario.save()
+    _usuario.senha_plana = senha
+    return _usuario
+
+
+@pytest.fixture()
+def resp_post(client, usuario):
+    resp = client.post(reverse('login'), {'username': usuario.email, 'password': usuario.senha_plana})
+    return resp
+
+
+def test_login_redirect(resp_post):
+    assert resp_post.status_code == 302
+    assert resp_post.url == reverse('modulos:indice')
