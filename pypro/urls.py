e982db136f57f0d81dad24847c83af293c6ea752
undersfx <atchin_@hotmail.com>
2021-03-07 19:05:40 -0300

    add: Formulário de login

--- pypro/urls.py
+++ pypro/urls.py
@@ -21,6 +21,7 @@ import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contas/', include('django.contrib.auth.urls')),
     path('', include('pypro.base.urls')),
     path('aperitivos/', include('pypro.aperitivos.urls')),
     path('modulos/', include('pypro.modulos.urls')),
