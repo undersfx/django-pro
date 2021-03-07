317fb3d672ec179cf2ca9b8f419ead0449fcbfc7
undersfx <atchin_@hotmail.com>
2021-03-07 19:45:58 -0300

    add: Botão de login ao Navbar

--- pypro/base/tests/test_login.py
+++ pypro/base/tests/test_login.py
@@ -1,6 +1,7 @@
 import pytest
 from django.urls import reverse
 from model_bakery import baker
+from pypro.django_assertions import assert_contains
 
 
 @pytest.fixture()
@@ -32,3 +33,17 @@ def resp_post(client, usuario):
 def test_login_redirect(resp_post):
     assert resp_post.status_code == 302
     assert resp_post.url == reverse('modulos:indice')
+
+
+@pytest.fixture()
+def resp_home(client, db):
+    resp = client.get(reverse('base:home'))
+    return resp
+
+
+def test_botao_entrar(resp_home):
+    assert_contains(resp_home, 'Entrar')
+
+
+def test_link_de_login(resp_home):
+    assert_contains(resp_home, reverse('login'))
